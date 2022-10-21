96e9cb67f4211e4bc56601fe87e8eea32afa077a
azepart <azepart15@yandex.ru>
2022-10-21 12:50:01 +0300

    Task03
    Task03

---
+++ Task03/hangman/bin/hangman.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+
+use function trapezaiv\hangman\Controller\key;
+
+if (isset($argv[1])) {
+    $key = $argv[1];
+    key($key);
+} else {
+    $key = "-n";
+    key($key);
+}
